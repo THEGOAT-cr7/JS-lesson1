e360b4fa9d28a060cb2d76fad7bd32f2a5e84539
Asadbek Tursunaliyev <tursunaliyev879@gmail.com>
2025-03-03 14:53:02 +0500

    first commit

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
-let name = prompt("Ismingizni kiriting:");
-let surname = prompt("Familyangizni kiriting:");
-let age = prompt("Yoshingizni kiriting:");
-alert(`Salom, ${name} ${surname}! Sizning yoshingiz ${age}.`);
+let password1 = prompt("Parolingizni kiriting:");
+let password2 = prompt("Parolingizni qayta kiriting:");
+console.log(`Parollar bir xilmi: ${password1 === password2}`);
 
-let num1 = Number(prompt("Birinchi sonni kiriting:"));
-let num2 = Number(prompt("Ikkinchi sonni kiriting:"));
-let sum = num1 + num2;
-alert(`Yig‘indi: ${sum}`); 
+let number = +prompt("Son kiriting:");
+console.log(`Juft son: ${number % 2 === 0}`);
 
-let num = Number(prompt("Biror son kiriting:"));
+let num = +prompt("Son kiriting:");
+console.log(
+  `Kvadrati: ${num ** 2}, Kubi: ${num ** 3}, Ildizi: ${Math.sqrt(num)}`
+);
 
-console.log(`Siz kiritgan son: ${num}`);
-console.log(`Kvadrati: ${num ** 2}`);
-console.log(`Kub: ${num ** 3}`);
+let x = +prompt("To'g'ri to'rtburchakning x tomonini kiriting:");
+let y = +prompt("To'g'ri to'rtburchakning y tomonini kiriting:");
+console.log(`Yuzi: ${x * y}, Perimetri: ${2 * (x + y)}`);
 
-alert(`Siz kiritgan son: ${num}\nKvadrati: ${num ** 2}\nKub: ${num ** 3}`);
+alert ("Bundan keyin eng katta va eng kichik sonni kiritasiz!");
+let a = +prompt("1-sonni kiriting:");
+let b = +prompt("2-sonni kiriting:");
+let c = +prompt("3-sonni kiriting:");
+console.log(
+    `Eng katta son: ${Math.max(a, b, c)}, Eng kichik son: ${Math.min(a, b, c)}`);
 
-let birthYear = Number(prompt("Tug‘ilgan yilingizni kiriting:"));
-let currentYear = new Date().getFullYear();
-let yearAge = currentYear - birthYear;
+alert ("Bundan keyin kub va kvadrat sonlar!");
+let num1 = +prompt("1-sonni kiriting:");
+let num2 = +prompt("2-sonni kiriting:");
+console.log(num1 ** 3 / num2 ** 2);
 
-console.log(`Sizning yoshingiz: ${yearAge}`);
-alert(`Sizning yoshingiz: ${yearAge}`);
+let dollar = +prompt("Dollar qiymatini kiriting:");
+console.log(`So'mda: ${dollar * 12000}`);
+
+let numA = +prompt("1-sonni kiriting:");
+let numB = +prompt("2-sonni kiriting:");
+console.log(numA / numB * 100);
+
+let minutes = +prompt("Minutni kiriting, men sekundga aylantiraman!:");
+console.log(`Sekundga aylantirilgan qiymat: ${minutes * 60}`);
