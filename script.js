40ceb228db2051c93cd40dce39b03e1cc5b31997
Asadbek Tursunaliyev <tursunaliyev879@gmail.com>
2025-03-03 14:57:52 +0500

    first commit

--- script.js
+++ script.js
@@ -2,36 +2,36 @@ let password1 = prompt("Parolingizni kiriting:");
 let password2 = prompt("Parolingizni qayta kiriting:");
 console.log(`Parollar bir xilmi: ${password1 === password2}`);
 
-let number = +prompt("Son kiriting:");
-console.log(`Juft son: ${number % 2 === 0}`);
+// let number = +prompt("Son kiriting:");
+// console.log(`Juft son: ${number % 2 === 0}`);
 
-let num = +prompt("Son kiriting:");
-console.log(
-  `Kvadrati: ${num ** 2}, Kubi: ${num ** 3}, Ildizi: ${Math.sqrt(num)}`
-);
+// let num = +prompt("Son kiriting:");
+// console.log(
+//   `Kvadrati: ${num ** 2}, Kubi: ${num ** 3}, Ildizi: ${Math.sqrt(num)}`
+// );
 
-let x = +prompt("To'g'ri to'rtburchakning x tomonini kiriting:");
-let y = +prompt("To'g'ri to'rtburchakning y tomonini kiriting:");
-console.log(`Yuzi: ${x * y}, Perimetri: ${2 * (x + y)}`);
+// let x = +prompt("To'g'ri to'rtburchakning x tomonini kiriting:");
+// let y = +prompt("To'g'ri to'rtburchakning y tomonini kiriting:");
+// console.log(`Yuzi: ${x * y}, Perimetri: ${2 * (x + y)}`);
 
-alert ("Bundan keyin eng katta va eng kichik sonni kiritasiz!");
-let a = +prompt("1-sonni kiriting:");
-let b = +prompt("2-sonni kiriting:");
-let c = +prompt("3-sonni kiriting:");
-console.log(
-    `Eng katta son: ${Math.max(a, b, c)}, Eng kichik son: ${Math.min(a, b, c)}`);
+// alert ("Bundan keyin eng katta va eng kichik sonni kiritasiz!");
+// let a = +prompt("1-sonni kiriting:");
+// let b = +prompt("2-sonni kiriting:");
+// let c = +prompt("3-sonni kiriting:");
+// console.log(
+//     `Eng katta son: ${Math.max(a, b, c)}, Eng kichik son: ${Math.min(a, b, c)}`);
 
-alert ("Bundan keyin kub va kvadrat sonlar!");
-let num1 = +prompt("1-sonni kiriting:");
-let num2 = +prompt("2-sonni kiriting:");
-console.log(num1 ** 3 / num2 ** 2);
+// alert ("Bundan keyin kub va kvadrat sonlar!");
+// let num1 = +prompt("1-sonni kiriting:");
+// let num2 = +prompt("2-sonni kiriting:");
+// console.log(num1 ** 3 / num2 ** 2);
 
-let dollar = +prompt("Dollar qiymatini kiriting:");
-console.log(`So'mda: ${dollar * 12000}`);
+// let dollar = +prompt("Dollar qiymatini kiriting:");
+// console.log(`So'mda: ${dollar * 12000}`);
 
-let numA = +prompt("1-sonni kiriting:");
-let numB = +prompt("2-sonni kiriting:");
-console.log(numA / numB * 100);
+// let numA = +prompt("1-sonni kiriting:");
+// let numB = +prompt("2-sonni kiriting:");
+// console.log(numA / numB * 100);
 
-let minutes = +prompt("Minutni kiriting, men sekundga aylantiraman!:");
-console.log(`Sekundga aylantirilgan qiymat: ${minutes * 60}`);
+// let minutes = +prompt("Minutni kiriting, men sekundga aylantiraman!:");
+// console.log(`Sekundga aylantirilgan qiymat: ${minutes * 60}`);
